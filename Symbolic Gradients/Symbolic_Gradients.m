9b9a31660e7cd6bc43ed741ad3acf8017994f6d7
Michael <67344799+MichaelTr7@users.noreply.github.com>
2020-10-28 01:21:40 -0400

    Update Symbolic_Gradients.m

--- Symbolic Gradients/Symbolic_Gradients.m
+++ Symbolic Gradients/Symbolic_Gradients.m
@@ -19,3 +19,15 @@ gradient(f(6))
  end
  
  
+%% 
+% syms w
+%  
+%  z_1h = [-1,-1,1,1,1,1]'; % a 6 by 1 vector
+%  z_2h = [0,1,0,0,1,1]';
+%  z_3h = [2,0,4,0,0,1]';
+%  f = (log(1+exp(-z_1h'*w))+log(1+exp(-z_2h'*w))+log(1+exp(-z_3h'*w))/3);
+%  
+% gradient(f,w);
+ 
+ 
+ 
